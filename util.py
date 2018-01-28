dd03ee3efe04b9e674da9cff3ac2d283687b4ba1
Abdul Aris <abdularisrahmanudin10@gmail.com>
2018-01-28 14:20:16 +0700

    Change importer utility module & add some documentations

--- util.py
+++ util.py
@@ -7,83 +7,139 @@ import json
 
 from unma.models import BaseTable, Admin, Department, Class, Student, Lecturer
 
+
 CSV_LECTURERS = 'data/dosen.csv'
 CSV_ADMIN = 'data/admin.csv'
 CSV_PUBLISHERS = 'data/publishers.csv'
 CSV_DEPARTMENTS = 'data/departments.csv'
+CLASSES_DESCRIPTOR = [
+    'data/classes/tif_kelas_kp_2017.json'
+]
 # CLASSES_DESCRIPTOR = [
 #     'data/classes/tii_a_2014.json',
 #     'data/classes/tif_a_2014.json',
 #     'data/classes/tim_a_2014.json',
 #     'data/classes/tis_a_2014.json'
 # ]
-CLASSES_DESCRIPTOR = [
-    'data/classes/tif_kelas_kp_2017.json'
-]
 
 
 def _write_json_config(dict_config, file_path):
     with open(file_path, 'w') as f:
         json.dump(dict_config, f)
-        print('\tConfig created: %s' % file_path)
+        print('\tBerkas config dibuat: %s' % file_path)
 
 
-def init_lecturers(db_session):
-    print("[*] Import akun dosen")
-    with open(CSV_LECTURERS) as csv_file:
+def import_lecturers(db_session, csv_filename, default_password='123'):
+    """
+    Import akun dosen dari data csv
+
+    Format csv:
+        separator ; (semicolon)
+        Nama Dosen;NIDN
+
+    :param db_session: session database
+    :param csv_filename: path ke berkas csv
+    :param default_password: default password untuk akun
+    """
+
+    print("[*] Import akun dosen %s" % csv_filename)
+    with open(csv_filename) as csv_file:
         data = csv.reader(csv_file, delimiter=';')
         for row in data:
             lect = Lecturer()
             lect.name = row[0]
             lect.username = row[1]
-            lect.password = '123'
+            lect.password = default_password
             db_session.add(lect)
-            print('\tAkun dosen ditambahkan: %s - %s' % (row[0], row[1]))
+            print('\t- Akun dosen ditambahkan: %s - %s' % (row[0], row[1]))
 
 
-def init_admin(db_session):
-    print("[*] Initialize admin account...")
-    with open(CSV_ADMIN) as csv_file:
+def import_admin(db_session, csv_filename, default_password='123'):
+    """
+    Import akun admin
+    Format csv: Name,Username
+
+    :param db_session: session database
+    :param csv_filename: path ke berkas csv
+    :param default_password: default password untuk akun
+    """
+
+    print("[*] Import akun admin %s" % csv_filename)
+    with open(csv_filename) as csv_file:
         data = csv.reader(csv_file)
         for row in data:
             admin = Admin()
             admin.name = row[0]
             admin.username = row[1]
-            admin.password = row[2]
+            admin.password = default_password
             admin.role = 'ADM'
             db_session.add(admin)
-            print('\tAdmin ditambahkan: %s' % admin.name)
+            print('\t- Admin ditambahkan: %s' % admin.name)
 
 
-def init_publishers(db_session):
-    print("[*] Initialize publisher accounts...")
-    with open(CSV_PUBLISHERS) as csv_file:
+def import_publishers(db_session, csv_filename, default_password='123'):
+    """
+    Import akun publisher
+    Format csv: Name,Username
+
+    :param db_session: session database
+    :param csv_filename: path ke berkas csv
+    :param default_password: default password untuk akun
+    """
+
+    print("[*] Import akun publisher %s" % csv_filename)
+    with open(csv_filename) as csv_file:
         data = csv.reader(csv_file)
         for row in data:
             publisher = Admin()
             publisher.name = row[0]
             publisher.username = row[1]
-            publisher.password = row[2]
+            publisher.password = default_password
             publisher.role = 'PUB'
             db_session.add(publisher)
-            print('\tPublisher ditambahkan: %s, %s' % (publisher.name, publisher.username))
+            print('\t- Publisher ditambahkan: %s, %s' % (publisher.name, publisher.username))
 
 
-def init_departments(db_session):
-    print("[*] Initialize departments table...")
-    with open(CSV_DEPARTMENTS) as csv_file:
+def import_departments(db_session, csv_filename):
+    """
+    Import data program studi
+    Format csv: Id Prodi,Nama Prodi
+
+    :param db_session: session database
+    :param csv_filename: path ke berkas csv
+    """
+    print("[*] Import akun prodi (departments) %s" % csv_filename)
+    with open(csv_filename) as csv_file:
         data = csv.reader(csv_file)
         for row in data:
             dep = Department()
             dep.id = row[0]
             dep.name = row[1]
             db_session.add(dep)
-            print('\tProdi ditambahkan: %s' % (dep.name))
+            print('\t- Prodi ditambahkan: %s' % (dep.name))
 
 
-def init_classes(db_session):
-    print("[*] Initialize classes and its students...")
-    for file_path in CLASSES_DESCRIPTOR:
+def import_classes(db_session, json_filename_list, student_default_password='123'):
+    """
+    Import data kelas berserta dengan akun mahasiswa-nya
+    Format json:
+    {
+        "id": angka unit sebagai id kelas,
+        "prodi_id": id untuk prodi kelas ini,
+        "name": nama kelas seperti 'A',
+        "year": tahung angkatan seperti 2014,
+        "type": tipe kelas 1 untuk regular dan 2 untuk karyawan,
+        "students_csv_path": path ke file csv yang berisi daftar data mahasiswa
+    }
+    Format csv untuk field students_csv_path: Nama,NPM
+
+    :param db_session: session database
+    :param json_filename_list: list untuk daftar path ke berkas json
+    :param student_default_password: default password untuk setiap akun mahasiswa
+    """
+
+    print("[*] Import data kelas dan mahasiswa")
+    for file_path in json_filename_list:
         with open(file_path, 'r') as file:
             c_json = json.loads(file.read())
             c = Class()
@@ -103,7 +159,7 @@ def init_classes(db_session):
                     stud.name = row[0]
                     stud.username = row[1]
                     stud.class_id = c.id
-                    stud.password = '123'
+                    stud.password = student_default_password
                     db_session.add(stud)
                     print('\t\t- Mahasiswa ditambahkan: %s, %s' % (stud.name, stud.username))
 
@@ -119,11 +175,11 @@ def gen_db(url_connection):
     session = sessionmaker(bind=engine)
     db_session = session()
 
-    init_admin(db_session)
-    init_publishers(db_session)
-    init_departments(db_session)
-    init_classes(db_session)
-    init_lecturers(db_session)
+    import_admin(db_session, CSV_ADMIN)
+    import_publishers(db_session, CSV_PUBLISHERS)
+    import_departments(db_session, CSV_DEPARTMENTS)
+    import_classes(db_session, CLASSES_DESCRIPTOR)
+    import_lecturers(db_session, CSV_LECTURERS)
 
     db_session.commit()
     cfg = {
